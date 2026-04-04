3567eec45e5568839e96552e2e5bf0403736d805
Mauricio Moreno <164837671+Mauricio0129@users.noreply.github.com>
2026-04-04 17:18:29 -0400

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        clean_string = []
+        for item in s:
+            if item.isalnum():
+                clean_string.append(item.lower())
+        clean_string = "".join(clean_string)
+        clean_string_reversed = clean_string[len(clean_string)-1::-1]
+        return clean_string == clean_string_reversed
